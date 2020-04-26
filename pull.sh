08ff5ac04d892122b035f8efa2583fcd73c45af8
任杰 <renjie@ascs.tech>
2020-04-26 14:45:08 +0800

    test

--- pull.sh
+++ pull.sh
@@ -9,5 +9,6 @@ docker login -u="$USERNAME" -p="$PASSWORD"
 for i in ${!imgs[@]} ; do
     echo "$i=>${imgs[i]}"
     docker pull ${imgs[i]}
-    echo "${imgs[i]}" | sed -e 's|registry.gitlab.com/gitlab-org/build/cng|penitence|g' | xargs -i docker push {}
+    fullname=$(echo "${imgs[i]}" | sed -e 's|registry.gitlab.com/gitlab-org/build/cng|penitence|g')
+    docker push $fullname
 done
