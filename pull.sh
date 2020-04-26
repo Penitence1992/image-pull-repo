7de8acae1ec82aa28aaf4a6c7052c05f647e5bc9
任杰 <renjie@ascs.tech>
2020-04-26 14:39:13 +0800

    test

--- pull.sh
+++ pull.sh
@@ -5,6 +5,9 @@ set -xue
 ECHO_IMAGES=$IMAGES
 IFS=' '
 imgs=($ECHO_IMAGES)
+docker login -u="$USERNAME" -p="$PASSWORD"
 for i in ${!imgs[@]} ; do
     echo "$i=>${imgs[i]}"
+    docker pull ${imgs[i]}
+    echo "${imgs[i]}" | sed -e 's|registry.gitlab.com/gitlab-org/build/cng|penitence|g' | xargs -i docker push {}
 done
