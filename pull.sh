fe83281788753538ab44a69c1e98b5c0a24d17b3
任杰 <renjie@ascs.tech>
2020-04-26 14:57:10 +0800

    fix

--- pull.sh
+++ pull.sh
@@ -10,5 +10,6 @@ for i in ${!imgs[@]} ; do
     echo "$i=>${imgs[i]}"
     docker pull ${imgs[i]}
     fullname=$(echo "${imgs[i]}" | sed -e 's|registry.gitlab.com/gitlab-org/build/cng|penitence|g')
+    docker tag ${imgs[i]} $fullname
     docker push $fullname
 done
