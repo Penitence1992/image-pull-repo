81aec87383d26c07df8a0e9429867f1eb6e1c64a
任杰 <renjie@ascs.tech>
2020-04-27 15:28:47 +0800

    feat: change to aliyun image server

--- pull.sh
+++ pull.sh
@@ -5,7 +5,7 @@ set -xue
 ECHO_IMAGES=$IMAGES
 IFS=' '
 imgs=($ECHO_IMAGES)
-docker login -u="$USERNAME" -p="$PASSWORD"
+docker login -u="$USERNAME" -p="$PASSWORD" registry.cn-hangzhou.aliyuncs.com
 for i in ${!imgs[@]} ; do
     echo "$i=>${imgs[i]}"
     docker pull ${imgs[i]}
