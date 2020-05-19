3ed80f796d583d6f7637bd355bb48f629b87283f
任杰 <renjie@ascs.tech>
2020-05-19 16:20:25 +0800

    feat: tianjia

--- pull.sh
+++ pull.sh
@@ -1,15 +1,14 @@
 #!/bin/bash
-
-
 set -xue
 ECHO_IMAGES=$IMAGES
 IFS=' '
 imgs=($ECHO_IMAGES)
+startWith="quay.io/fluentd_elasticsearch"
 docker login -u="$USERNAME" -p="$PASSWORD"
 for i in ${!imgs[@]} ; do
     echo "$i=>${imgs[i]}"
     docker pull ${imgs[i]}
-    fullname=$(echo "${imgs[i]}" | sed -e 's|registry.gitlab.com/gitlab-org/build/cng|rpenitence|g')
+    fullname=$(echo "${imgs[i]}" | sed -e 's|${startWith}|penitence|g')
     docker tag ${imgs[i]} $fullname
     docker push $fullname
 done
