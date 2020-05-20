1ad9d340b5100d8d753809c05641265d6585fdb6
任杰 <renjie@ascs.tech>
2020-05-20 08:53:18 +0800

    fix: fix pull shell

--- pull.sh
+++ pull.sh
@@ -3,12 +3,11 @@ set -xue
 ECHO_IMAGES=$IMAGES
 IFS=' '
 imgs=($ECHO_IMAGES)
-startWith="quay.io/fluentd_elasticsearch"
 docker login -u="$USERNAME" -p="$PASSWORD"
 for i in ${!imgs[@]} ; do
     echo "$i=>${imgs[i]}"
     docker pull ${imgs[i]}
-    fullname=$(echo "${imgs[i]}" | sed -e 's|${startWith}|penitence|g')
+    fullname=$(echo "${imgs[i]}" | sed -e 's|quay.io/fluentd_elasticsearch|penitence|g')
     docker tag ${imgs[i]} $fullname
     docker push $fullname
 done
