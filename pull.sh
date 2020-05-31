69e74b8ece02bb4dde5398af4fe15a0ce01400b7
任杰 <renjie@ascs.tech>
2020-05-31 18:48:34 +0800

    feat: 恢复为aliyun

--- pull.sh
+++ pull.sh
@@ -3,11 +3,11 @@ set -xue
 ECHO_IMAGES=$IMAGES
 IFS=' '
 imgs=($ECHO_IMAGES)
-docker login -u="$USERNAME" -p="$PASSWORD"
+docker login -u="$USERNAME" -p="$PASSWORD" registry.cn-hangzhou.aliyuncs.com
 for i in ${!imgs[@]} ; do
     echo "$i=>${imgs[i]}"
     docker pull ${imgs[i]}
-    fullname=$(echo "${imgs[i]}" | sed -e 's|quay.io/fluentd_elasticsearch|penitence|g')
+    fullname=$(echo "${imgs[i]}" | sed -e 's|registry.gitlab.com/gitlab-org/build/cng|registry.cn-hangzhou.aliyuncs.com/penitence|g')
     docker tag ${imgs[i]} $fullname
     docker push $fullname
 done
