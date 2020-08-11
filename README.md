38b230abc001a8aea35d51217eb4ec5f2b097749
任杰 <renjie@ascs.tech>
2020-08-11 13:26:10 +0800

    feat: 提供更多的定制功能

---
+++ README.md
@@ -0,0 +1,12 @@
+# 国外镜像拉取脚本
+
+## 说明
+
+参数 | 说明 
+:---:|:---:
+REGISTRY | 同步到的远程host
+SOURCE_NAME | 原始名称前缀
+TARGET_NAME | 目标名称前缀
+IMAGES | 镜像列表, 空格分开多个
+USERNAME | 登录用的用户名
+PASSWORD | 登录用的密码
--- pull.sh
+++ pull.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
 set -xue
 ECHO_IMAGES=$IMAGES
+SYNC_HOST=$REGISTRY
+SOURCE_NAME=$SOURCE_NAME
+TARGET_NAME=$TARGET_NAME
+# change image name $SOURCE_NAME/<image_name>:<tag> to $SYNC_HOST/$TARGET_NAME/<image_name>:<tag>
+# eg registry.gitlab.com/gitlab-org/build/cng/abc:latest registry.cn-hangzhou.aliyuncs.com/penitence/abc:latest
 IFS=' '
 imgs=($ECHO_IMAGES)
-docker login -u="$USERNAME" -p="$PASSWORD" registry.cn-hangzhou.aliyuncs.com
+
+if [ -z "$REGISTRY" ]; then
+ echo "param REGISTRY must set"
+ exit 1
+fi
+
+if [ ! -z "$USERNAME" ]; then
+    docker login -u="$USERNAME" -p="$PASSWORD" $SYNC_HOST
+fi
+
 for i in ${!imgs[@]} ; do
     echo "$i=>${imgs[i]}"
     docker pull ${imgs[i]}
-    fullname=$(echo "${imgs[i]}" | sed -e 's|registry.gitlab.com/gitlab-org/build/cng|registry.cn-hangzhou.aliyuncs.com/penitence|g')
+    fullname=$(echo "${imgs[i]}" | sed -e "s|$SOURCE_NAME|$SYNC_HOST/$TARGET_NAME|g")
     docker tag ${imgs[i]} $fullname
     docker push $fullname
 done
